88a9c1277b52c79088527c1bbf809a5d10a25aca
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-09-06 03:10:09 +0300

    commit tree 3f9d90ab5a033fc7643c915d66360bfebe0043c4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
   # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.by_price
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    most_expensive = BakedGood.most_expensive
+    most_expensive.to_json
+  end
 
 end
